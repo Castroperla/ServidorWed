460d401ccff20721ff0ebcbc582a646a355e0eb8
CastroPerla <perlacass936@gmail.com>
2023-08-21 08:58:31 -0600

    agregamos platillas

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const http = require('http')
-const port = 5000 
+const express= require('express')
+const app = express()
+const port = process.env.PORT || 5000 
 
-const server = http.createServer((req, res) => {
-    res.end('Hola Alumnos')
-})
+//app.use(express.static(__dirname + '/public'))
 
-server.listen(port, () => {
-    console.log('Arranca el servidor')
+//Motor de Plantillas o vistas 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/'), (req, res) => {
+    res.render('index', {titulo: 'Index con EJS', 
+    })
+}
+
+app.get('/Lenguajes'), (req, res) => {
+    res.sendFile(__dirname + '/public7index.html')
+}
+
+app.listen(port, () => {
+    console.log('Arranca el servidor',  port)
 })
